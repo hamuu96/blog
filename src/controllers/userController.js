464a7cb70f561172404070057e2b182b10c30c5d
hamuu96 <hamanto96@gmail.com>
2022-02-21 02:33:20 +0000

    user signup form validate if user exists in database

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -16,22 +16,39 @@ exports.signup = (req, res, next) => {
     
     const { firstname, lastname, email, password, address, author, dob } = req.body;
     
-
-
-
     const isoDate = new Date(dob);
     const mySQLDateString = isoDate.toJSON().slice(0, 19).replace('T', ' ');
+    console.log();
 
-    bcrypt.genSalt(5, async function(err, salt) {
-        bcrypt.hash(password, salt, function(err, hash) {
-            // Store hash in your password DB.
+    //check if user exists
+    sqlController.getUser(email, (result, err) =>{
+        // res.json({msg: result.length})
+        if(result.length > 0){
+            res.json({err: 'user already exists'})
+        }else{
+             //check if password is longer than 8 characters
+            if(password.length <= 8){
+                res.json({err: 'please enter a longer password'})
+            }
+            else{
+            //generate salt and encrypt password  
+                bcrypt.genSalt(5, async function(err, salt) {
+                    bcrypt.hash(password, salt, function(err, hash) {
+                        //insert user into user table
+                        sqlController.insertUsers(firstname, lastname, email, hash, address, author, mySQLDateString, (result, err) =>{
+                            if(err) throw err;
+                            res.status(200);
+                            res.redirect('/main');
+                            // res.json({msg: 'success'});
+                        });
+                    });
+                });
+            } 
+        }
+       
+    }) 
+      
 
-            sqlController.insertUsers(firstname, lastname, email, hash, address, author, mySQLDateString, (result, err) =>{
-                if(err) throw err;
-                res.json({msg: 'success'});
-            });
-        });
-    });
-
-    
+   
+   
 }
